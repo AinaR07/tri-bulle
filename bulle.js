3f7851f507c07ed950b2534a8ba3089dd89dd08d
AinaR07 <hei.aina.59@gmail.com>
2022-05-09 20:39:37 +0200

    second commit

--- bulle.js
+++ bulle.js
@@ -3,14 +3,14 @@
 
 
 
-function bulle(tab) {
-    for (let i=0; i<tab.length-1; i++){
-        if(tab[i] > tab[i+1]){
-            let p = tab[i];
-            tab[i] = tab[i+1];
-            tab[i+1] = p;
+function bulle(tableau) {
+    for (let i=0; i<tableau.length-1; i++){
+        if(tableau[i] > tableau[i+1]){
+            let p = tableau[i];
+            tableau[i] = tableau[i+1];
+            tableau[i+1] = p;
         }
-    }return tab; 
+    }return tableau; 
 }
-let tab = [ 5, 4 , 3 , 1]
-console.log(bulle(tab));
+let tableau = [ 5, 4 , 3 , 1]
+console.log(bulle(tableau));
